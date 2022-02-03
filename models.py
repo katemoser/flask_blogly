6b8cf377b02d1689354916a35748b082d7168758
Kate Moser <komoser@gmail.com>
2022-02-03 17:23:05 -0600

    created Post Model and templates for post functionality

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, func
+
+
 DEFAULT_IMAGE_URL = "https://media.wired.co.uk/photos/607d91994d40fbb952b6ad64/4:3/w_2664,h_1998,c_limit/wired-meme-nft-brian.jpg"
 db = SQLAlchemy()
+
 
 def connect_db(app):
     """Connect to database"""
@@ -13,11 +16,30 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer,
-                    primary_key=True,
-                    autoincrement=True)
+                   primary_key=True,
+                   autoincrement=True)
     first_name = db.Column(db.String(50),
-                            nullable=False)
+                           nullable=False)
     last_name = db.Column(db.String(50),
-                            nullable=False)
+                          nullable=False)
     image_url = db.Column(db.Text,
-                            nullable=True)
+                          nullable=True)
+
+
+class Post(db.Model):
+    """Post"""
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime,
+                           timezone=True,
+                           default=func.current_timestamp())
+    user_id = db.Column(db.ForeignKey("users.id"))
+
+    user = db.relationship('User',
+                           backref='posts')
